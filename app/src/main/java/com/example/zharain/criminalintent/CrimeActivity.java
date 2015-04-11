624e8a703f4eeb71d5f65094ac2d8c9d0f3c9480
rainzha <rainzha@163.com>
2015-04-11 11:17:30 +0800

    finished Chapter07.

--- app/src/main/java/com/example/zharain/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/zharain/criminalintent/CrimeActivity.java
@@ -1,8 +1,9 @@
 package com.example.zharain.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -10,5 +11,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
