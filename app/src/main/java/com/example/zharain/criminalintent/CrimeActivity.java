36218128b1596e86034ebcda598281e2312d33d7
rainzha <rainzha@163.com>
2015-04-11 11:23:08 +0800

    finished Chapter07Exercise.

--- app/src/main/java/com/example/zharain/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/zharain/criminalintent/CrimeActivity.java
@@ -1,18 +1,17 @@
 package com.example.zharain.criminalintent;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
-        FragmentManager fm = getSupportFragmentManager();
+        android.app.FragmentManager fm = getFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
         if (fragment == null) {
--- app/src/main/java/com/example/zharain/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/zharain/criminalintent/CrimeFragment.java
@@ -1,8 +1,8 @@
 package com.example.zharain.criminalintent;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
