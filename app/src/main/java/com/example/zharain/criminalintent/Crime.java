d60dce5e3edc9a7e370535b5acdf36d491d7f919
rainzha <rainzha@163.com>
2015-04-11 11:52:27 +0800

    updated CrimeFragment layout in Chapter08.

--- app/src/main/java/com/example/zharain/criminalintent/Crime.java
+++ app/src/main/java/com/example/zharain/criminalintent/Crime.java
@@ -1,10 +1,29 @@
 package com.example.zharain.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
