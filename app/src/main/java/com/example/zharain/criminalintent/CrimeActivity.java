94e0bd30bd9604a0616c7b0b12f2f045c40bd52f
rainzha <rainzha@163.com>
2015-04-22 18:49:38 +0800

    Send argument info to CrimeFragment in Chapter10.

--- app/src/main/java/com/example/zharain/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/zharain/criminalintent/CrimeActivity.java
@@ -2,9 +2,13 @@ package com.example.zharain.criminalintent;
 
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 }
