3ca48f676822ab67f7e6f4b77ab1213f8c48d70f
zharain <zharain@zharaindeMacBook-Pro.local>
2015-04-24 19:54:07 +0800

    finished in Chapter12.

--- app/src/main/java/com/example/zharain/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/zharain/criminalintent/DatePickerFragment.java
@@ -1,23 +1,93 @@
 package com.example.zharain.criminalintent;
 
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE = "com.example.zharain.criminalintent.date";
+    private Date mDate;
+
+    private void sendResult(int resultCode) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, mDate);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+        mDate = (Date) getArguments().getSerializable(EXTRA_DATE);
 
-        return new AlertDialog.Builder(getActivity())
-                .setView(v)
-                .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
-                .create();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {   //Use DatePickerDialog on L and Lollipop machines
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+                    sendResult(Activity.RESULT_OK);
+                }
+            }, year, month, day);
+            dialog.setTitle(R.string.date_picker_title);
+            return dialog;
+
+        } else {
+            View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
+            DatePicker datePicker = (DatePicker) v.findViewById(R.id.dialog_date_datePicker);
+            datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+                @Override
+                public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+
+                    getArguments().putSerializable(EXTRA_DATE, mDate);
+                }
+            });
+
+            return new AlertDialog.Builder(getActivity())
+                    .setView(v)
+                    .setTitle(R.string.date_picker_title)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            sendResult(Activity.RESULT_OK);
+                        }
+                    })
+                    .create();
+        }
+    }
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
     }
 }
